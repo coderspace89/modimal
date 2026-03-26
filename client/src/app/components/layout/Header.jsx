7e62c841d7dfe57570a6aea536228eec00162b78
coderspace89 <coderspace89@gmail.com>
2026-03-26 19:08:13 +0500

    deskop header is completed mobile menu is remaining

--- client/src/app/components/layout/Header.jsx
+++ client/src/app/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import headerStyles from "./Header.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -15,6 +15,7 @@ import { usePathname } from "next/navigation";
 const Header = () => {
   const [headerData, setHeaderData] = useState(null);
   const [navigationData, setNavigationData] = useState(null);
+  const [openId, setOpenId] = useState(null);
 
   const headerQuery = qs.stringify(
     {
@@ -82,6 +83,26 @@ const Header = () => {
     fetchNavigation();
   }, []);
 
+  const closeTimeout = useRef(null);
+
+  const handleMouseEnter = (id) => {
+    if (closeTimeout.current) {
+      clearTimeout(closeTimeout.current);
+      closeTimeout.current = null;
+    }
+    setOpenId(id);
+  };
+
+  const handleMouseLeave = () => {
+    closeTimeout.current = setTimeout(() => {
+      setOpenId(null);
+    }, 500); // 5 seconds
+  };
+
+  useEffect(() => {
+    return () => clearTimeout(closeTimeout.current);
+  }, []);
+
   return (
     <section>
       <div className={headerStyles.topbarContainer}>
@@ -102,33 +123,112 @@ const Header = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto ms-auto">
-              {navigationData?.map((navItem) => (
-                <NavDropdown
-                  key={navItem.id}
-                  title={navItem.label}
-                  id="megamenu-dropdown"
-                  className={headerStyles.megamenu}
-                >
-                  {navItem?.Megamenu?.length > 0 &&
-                    navItem?.Megamenu?.map((menuItem) => (
-                      <div key={menuItem.id} className={headerStyles.megacol}>
-                        <h4>{menuItem?.columnTitle}</h4>
-                        {/* Text links */}
-                        {menuItem.links?.length > 0 && (
-                          <div>
-                            {menuItem.links.map((link) => (
-                              <NavDropdown.Item key={link.id} href={link.url}>
-                                {link.label}
-                              </NavDropdown.Item>
-                            ))}
+            <header className={headerStyles.headerContainer}>
+              <nav className={`${headerStyles.nav} ms-auto me-auto`}>
+                <ul className={headerStyles.navlist}>
+                  {navigationData?.map((navItem) => (
+                    <li
+                      key={navItem.id}
+                      className={headerStyles.navitem}
+                      onMouseEnter={() => handleMouseEnter(navItem.id)}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <Link href={navItem.url} className={headerStyles.navlink}>
+                        {navItem.label}
+                      </Link>
+
+                      {navItem.hasMegamenu &&
+                        navItem.Megamenu?.length > 0 &&
+                        openId === navItem.id && (
+                          <div
+                            className={headerStyles.megamenu}
+                            onMouseEnter={() => handleMouseEnter(navItem.id)}
+                            onMouseLeave={handleMouseLeave}
+                          >
+                            <div className={headerStyles.megamenuInner}>
+                              {navItem.Megamenu.map((menuItem) => (
+                                <div
+                                  key={menuItem.id}
+                                  className={headerStyles.megacolContainer}
+                                >
+                                  <h4 className={headerStyles.columnTitle}>
+                                    {menuItem.columnTitle}
+                                  </h4>
+                                  <div className={headerStyles.megacol}>
+                                    {menuItem.links?.length > 0 && (
+                                      <ul className={headerStyles.linklist}>
+                                        {menuItem.links.map((link) => (
+                                          <li key={link.id}>
+                                            <Link href={link.url}>
+                                              {link.label}
+                                            </Link>
+                                          </li>
+                                        ))}
+                                      </ul>
+                                    )}
+
+                                    {menuItem.featuredItems?.length > 0 && (
+                                      <div
+                                        className={
+                                          menuItem.featuredItems?.length <= 2
+                                            ? headerStyles.featuredgrid1
+                                            : headerStyles.featuredgrid
+                                        }
+                                      >
+                                        {menuItem.featuredItems.map((item) => (
+                                          <Link
+                                            key={item.id}
+                                            href={item.url || "/"}
+                                            className={
+                                              headerStyles.featuredcard
+                                            }
+                                          >
+                                            <Image
+                                              src={getStrapiMedia(
+                                                item.image.url,
+                                              )}
+                                              width={item.image.width}
+                                              height={item.image.height}
+                                              alt={item.image.name}
+                                            />
+                                            {item.title && (
+                                              <span>{item.title}</span>
+                                            )}
+                                          </Link>
+                                        ))}
+                                      </div>
+                                    )}
+                                  </div>
+                                </div>
+                              ))}
+                            </div>
                           </div>
                         )}
-                      </div>
-                    ))}
-                </NavDropdown>
-              ))}
-            </Nav>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </header>
+            <header className={headerStyles.headerContainer}>
+              <nav className={`${headerStyles.nav} ms-auto me-auto`}>
+                <ul className={headerStyles.navlist}>
+                  {headerData?.headerIcons.map((headerIcon) => (
+                    <li key={headerIcon.id}>
+                      <Link href={headerIcon.url}>
+                        {headerIcon?.icon && (
+                          <Image
+                            src={getStrapiMedia(headerIcon.icon.url)}
+                            width={headerIcon.icon.width}
+                            height={headerIcon.icon.height}
+                            alt={headerIcon.icon.name}
+                          />
+                        )}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </header>
           </Navbar.Collapse>
         </Container>
       </Navbar>
